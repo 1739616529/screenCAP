37f89c241045a4d643f5082784111c99b71b6f73
1739616529 <1739616529@qq.com>
2022-05-10 05:44:00 +0800

    组检测试

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,52 +1,165 @@
-import React, { useState } from "react"
+import React, { useState, Fragment } from "react"
+import { Disclosure, Menu, Transition } from "@headlessui/react"
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import TitleBar from "@/components/TitleBar"
 import { observer } from "mobx-react"
 import { useStore } from '@/store'
+
 function Home(): JSX.Element {
 	const { count_list, filter_count_list } = useStore()['RootStore']
-	const [count, set_count] = useState(count_list.length)
-	function test_btn() {
-		set_count(() => {
-			return filter_count_list().length
-		})
+
+	function classNames(...classes: string[]) {
+		const ccc = classes.filter(Boolean).join(' ')
+		console.log(classes)
+		return ccc
 	}
 
-	const people = [
-		{
-			name: 'Calvin Hawkins',
-			email: 'calvin.hawkins@example.com',
-			image:
-				'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		},
-		{
-			name: 'Kristen Ramos',
-			email: 'kristen.ramos@example.com',
-			image:
-				'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		},
-		{
-			name: 'Ted Fox',
-			email: 'ted.fox@example.com',
-			image:
-				'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		},
+	const navigation = [
+		{ name: 'Dashboard', href: '#', current: true },
+		{ name: 'Team', href: '#', current: true },
+		{ name: 'Projects', href: '#', current: false },
+		{ name: 'Calendar', href: '#', current: false },
 	]
+
 
 	return (
 		<div className="Home">
-			Home Page
-			<TitleBar />
-			<p>{count}</p>
-			<button onClick={test_btn} >点击切换 </button>
-			{people.map((person) => (
-				<li key={person.email} className="py-4 flex">
-					<img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-					<div className="ml-3">
-						<p className="text-sm font-medium text-gray-900">{person.name}</p>
-						<p className="text-sm text-gray-500">{person.email}</p>
-					</div>
-				</li>
-			))}
+			<Disclosure as="nav" className="bg-gray-800">
+				{({ open }) => (
+					<>
+						<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+							<div className="relative flex items-center justify-between h-16">
+								<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+									{/* Mobile menu button*/}
+									<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+										<span className="sr-only">Open main menu</span>
+										{open ? (
+											<XIcon className="block h-6 w-6" aria-hidden="true" />
+										) : (
+											<MenuIcon className="block h-6 w-6" aria-hidden="true" />
+										)}
+									</Disclosure.Button>
+								</div>
+								<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+									<div className="flex-shrink-0 flex items-center">
+										<img
+											className="block lg:hidden h-8 w-auto"
+											src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+											alt="Workflow"
+										/>
+										<img
+											className="hidden lg:block h-8 w-auto"
+											src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+											alt="Workflow"
+										/>
+									</div>
+									<div className="hidden sm:block sm:ml-6">
+										<div className="flex space-x-4">
+											{navigation.map((item) => (
+												<a
+													key={item.name}
+													href={item.href}
+													className={classNames(
+														item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+														'px-3 py-2 rounded-md text-sm font-medium'
+													)}
+													aria-current={item.current ? 'page' : undefined}
+												>
+													{item.name}1
+												</a>
+											))}
+										</div>
+									</div>
+								</div>
+								<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+									<button
+										type="button"
+										className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+									>
+										<span className="sr-only">View notifications</span>
+										<BellIcon className="h-6 w-6" aria-hidden="true" />
+									</button>
+
+									{/* Profile dropdown */}
+									<Menu as="div" className="ml-3 relative">
+										<div>
+											<Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+												<span className="sr-only">Open user menu</span>
+												<img
+													className="h-8 w-8 rounded-full"
+													src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+													alt=""
+												/>
+											</Menu.Button>
+										</div>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href="#"
+															className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+														>
+															Your Profile
+														</a>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href="#"
+															className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+														>
+															Settings
+														</a>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href="#"
+															className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+														>
+															Sign out
+														</a>
+													)}
+												</Menu.Item>
+											</Menu.Items>
+										</Transition>
+									</Menu>
+								</div>
+							</div>
+						</div>
+
+						<Disclosure.Panel className="sm:hidden">
+							<div className="px-2 pt-2 pb-3 space-y-1">
+								{navigation.map((item) => (
+									<Disclosure.Button
+										key={item.name}
+										as="a"
+										href={item.href}
+										className={classNames(
+											item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+											'block px-3 py-2 rounded-md text-base font-medium'
+										)}
+										aria-current={item.current ? 'page' : undefined}
+									>
+										{item.name}
+									</Disclosure.Button>
+								))}
+							</div>
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
 		</div>
 	)
 }
