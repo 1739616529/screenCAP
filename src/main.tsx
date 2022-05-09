1c62109052fa37745efa4dfd495874bc33e25285
1739616529 <1739616529@qq.com>
2022-05-10 05:41:12 +0800

    删除 amin.tsx引入的本地字体css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import '../public/font/interFont.css'
 const app = ReactDOM.createRoot(document.getElementById('root')!);
 app.render(
   <React.StrictMode>
