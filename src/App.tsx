ab42f2b38eb55b3714c60925e12b92056b3f64c5
1739616529 <1739616529@qq.com>
2022-05-10 05:13:24 +0800

    删除多余 mobx组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Provider } from 'mobx-react'
-import { stores, StoresContext } from './store'
 import Home from './page/Home/Home'
-console.log(stores)
-console.log(StoresContext)
 function App() {
 
   return (
     <div className="App">
-      <Provider {...stores}>
-        <StoresContext.Provider value={stores}>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Home />} />
-            </Routes>
-          </BrowserRouter>
-        </StoresContext.Provider>
-      </Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
