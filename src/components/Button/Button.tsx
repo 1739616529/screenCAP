56db6a5fb3b82e928e9509169f3a4fc0a4696b10
1739616529 <1739616529@qq.com>
2022-05-14 12:08:26 +0800

    修改自定义类名

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,10 @@ import React from "react"
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> { }
 export default function Button(props: ButtonProps): JSX.Element {
 
+	function create_className(...args: any[]) {
+		return args.join(' ')
+	}
 	return (
-		<button type="button" {...props} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"></button>
+		<button type="button" {...props} className={create_className("text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 ring-0 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 outline-none dark:focus:ring-blue-800", props.className)}></button>
 	)
 }
